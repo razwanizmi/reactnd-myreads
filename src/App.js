fe9c3d8ab6c2a8978bb766762926435a785f995d
Razwan Rashidi <razwan.izmi@gmail.com>
2017-09-14 00:17:25 +0800

    Add loading

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { Route } from "react-router-dom";
+import Loading from "./components/Loading";
 import Main from "./components/Main";
 import Search from "./components/Search";
 import * as BooksAPI from "./utils/BooksAPI";
@@ -7,14 +8,19 @@ import "./App.css";
 
 class BooksApp extends Component {
   state = {
-    books: []
+    books: [],
+    loading: true
   };
 
   componentDidMount() {
-    BooksAPI.getAll().then(books => this.setState({ books }));
+    BooksAPI.getAll().then(books => this.setState({ books, loading: false }));
   }
 
   render() {
+    if (this.state.loading) {
+      return <Loading />;
+    }
+
     return (
       <div className="app">
         <Route
