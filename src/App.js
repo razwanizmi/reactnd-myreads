77c0044ebe6bf4b0c804836fc5d6549c86244420
Razwan Rashidi <razwan.izmi@gmail.com>
2017-09-14 19:56:44 +0800

    Basic search component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import { Route } from "react-router-dom";
 import Main from "./components/Main";
 import Search from "./components/Search";
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,8 +1,39 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import Book from "./Book";
+import * as BooksAPI from "../utils/BooksAPI";
 
 class Search extends Component {
+  state = {
+    term: "",
+    books: []
+  };
+
+  handleChange = event => {
+    const term = event.target.value;
+
+    this.setState({ term });
+    this.searchTerm(term);
+  };
+
+  searchTerm = term => {
+    if (term === "") {
+      this.setState({ books: [] });
+    }
+
+    if (term !== "") {
+      BooksAPI.search(term).then(books => {
+        if (Array.isArray(books)) {
+          return this.setState({ books });
+        }
+        this.setState({ books: [] });
+      });
+    }
+  };
+
   render() {
+    const { term, books } = this.state;
+
     return (
       <div className="search-books">
         <div className="search-books-bar">
@@ -10,11 +41,18 @@ class Search extends Component {
             Close
           </Link>
           <div className="search-books-input-wrapper">
-            <input type="text" placeholder="Search by title or author" />
+            <input
+              type="text"
+              placeholder="Search by title or author"
+              value={term}
+              onChange={this.handleChange}
+            />
           </div>
         </div>
         <div className="search-books-results">
-          <ol className="books-grid" />
+          <ol className="books-grid">
+            {books.map(book => <Book key={book.id} book={book} />)}
+          </ol>
         </div>
       </div>
     );
